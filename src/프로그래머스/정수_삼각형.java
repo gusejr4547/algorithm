28251fbf7dac740eff1708e8fdb55b6ca42e1790
조현덕 <gusejr4547@gmail.com>
2023-11-29 15:22:55 +0900

    정수_삼각형

---
+++ src/프로그래머스/정수_삼각형.java
@@ -0,0 +1,17 @@
+package 프로그래머스;
+
+public class 정수_삼각형 {
+    public static void main(String[] args) {
+        int[][] triangle = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+        정수_삼각형 Main = new 정수_삼각형();
+        System.out.println(Main.solution(triangle));
+    }
+
+    public int solution(int[][] triangle) {
+
+
+        int answer = 0;
+
+        return answer;
+    }
+}
