e1d05bb6a7c9eab7c84ae2293aa82d33b4ecf693
조현덕 <gusejr4547@gmail.com>
2023-12-14 14:13:23 +0900

    P2056_작업

--- src/백준/P2056_작업.java
+++ src/백준/P2056_작업.java
@@ -10,14 +10,14 @@ import java.util.StringTokenizer;
 public class P2056_작업 {
     static List<Integer>[] adj;
     static int[] taskCostArray;
-    static int[] visit;
+    static int[] indegree;
     static ArrayDeque<Integer> stack = new ArrayDeque<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
         taskCostArray = new int[N + 1];
-        visit = new int[N + 1];
+        indegree = new int[N + 1];
         adj = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++) {
             adj[i] = new ArrayList<>();
@@ -31,25 +31,44 @@ public class P2056_작업 {
             while (st.hasMoreTokens()) {
                 int task = Integer.parseInt(st.nextToken());
                 adj[task].add(i);
+                indegree[i]++;
             }
         }
 
-        System.out.println(topologySort(1));
+        System.out.println(topologySort(N));
     }
 
-    static int topologySort(int node) {
-        List<Integer> nextNodeList = adj[node];
+    static int topologySort(int N) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
 
-        int answer = 0;
-        for (Integer nextNode : nextNodeList) {
-            if (visit[nextNode] == 0) {
-                answer = Math.max(answer, topologySort(nextNode));
-            }else{
-                answer = Math.max(answer, visit[nextNode]);
+        int[] result = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            result[i] = taskCostArray[i];
+
+            if (indegree[i] == 0) {
+                queue.offer(i);
             }
         }
-//        System.out.println(node + ", " + (taskCostArray[node] + answer));
 
-        return taskCostArray[node] + answer;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            for (int next : adj[curr]) {
+                indegree[next]--;
+
+                result[next] = Math.max(result[next], result[curr] + taskCostArray[next]);
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            answer = Math.max(answer, result[i]);
+        }
+
+        return answer;
     }
 }
