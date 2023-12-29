8b4e2db1de4ff01f69cde3f508290bfe8bbdc8bc
조현덕 <gusejr4547@gmail.com>
2023-12-29 17:51:49 +0900

    P4195_친구_네트워크

---
+++ src/백준/P4195_친구_네트워크.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P4195_친구_네트워크 {
+    static int T;
+
+    // 유니온 - 파인드
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        for (int test = 0; test < T; test++) {
+
+        }
+    }
+}
