1a2efb1cfbac9f4b86dfe6bdd2870579d25b8c9c
조현덕 <gusejr4547@gmail.com>
2024-01-09 17:45:01 +0900

    P20183_골목_대장_호석_효율성2 - 이분탐색 + 다익스트라

--- src/백준/P20183_골목_대장_호석_효율성2.java
+++ src/백준/P20183_골목_대장_호석_효율성2.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 이분탐색 + 다익스트라
+
 public class P20183_골목_대장_호석_효율성2 {
     static int N, M;
     static long C;
@@ -23,67 +25,82 @@ public class P20183_골목_대장_호석_효율성2 {
             adj[i] = new ArrayList<>();
         }
 
+        int max = 0;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
+            max = Math.max(max, c);
+
             adj[a].add(new Node(b, c));
             adj[b].add(new Node(a, c));
         }
 
-        System.out.println(dijkstra(start, end));
+        int answer = -1;
+        int left = 0;
+        // 1개 골목의 최대값
+        int right = max;
+        while (left <= right) {
+            int maxCost = (left + right) / 2;
+            long[] distance = dijkstra(start, maxCost);
+//            System.out.println(Arrays.toString(distance));
+
+            // 경로 없음
+            if (distance[end] == Long.MAX_VALUE) {
+                left = maxCost + 1;
+            } else {
+                right = maxCost - 1;
+                answer = maxCost;
+            }
+        }
+        System.out.println(answer);
     }
 
-    public static long dijkstra(int start, int end) {
+    public static long[] dijkstra(int start, int maxCost) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         long[] distance = new long[N + 1];
-        Arrays.fill(distance, Long.MAX_VALUE);
-        pq.offer(new Node(start, 0, 0));
-        distance[start] = 0;
         boolean[] visit = new boolean[N + 1];
-        visit[start] = true;
+        Arrays.fill(distance, Long.MAX_VALUE);
+        pq.offer(new Node(start, 0));
+        distance[start] = 0;
 
         while (!pq.isEmpty()) {
             Node curr = pq.poll();
             int currNum = curr.nodeNum;
             long currCost = curr.cost;
-            long currMax = curr.maxCost;
+
+            if (visit[currNum]) continue;
+            visit[currNum] = true;
 
             for (Node next : adj[currNum]) {
-                if (currCost + next.cost > C || visit[next.nodeNum]) continue;
-                if (distance[next.nodeNum] > Math.max(currMax, next.cost)) {
-                    distance[next.nodeNum] = Math.max(currMax, next.cost);
-                    visit[next.nodeNum] = true;
-                    pq.offer(new Node(next.nodeNum, currCost + next.cost, distance[next.nodeNum]));
+                if (next.cost > maxCost) continue;
+
+                long sum = currCost + next.cost;
+                if (sum > C || visit[next.nodeNum]) continue;
+                if (distance[next.nodeNum] > sum) {
+                    distance[next.nodeNum] = sum;
+                    pq.offer(new Node(next.nodeNum, sum));
                 }
             }
         }
-
-        return distance[end] != Long.MAX_VALUE ? distance[end] : -1;
+        return distance;
     }
 
 
     public static class Node implements Comparable<Node> {
         int nodeNum;
         long cost;
-        long maxCost;
 
         public Node(int nodeNum, long cost) {
             this.nodeNum = nodeNum;
             this.cost = cost;
         }
 
-        public Node(int nodeNum, long cost, long maxCost) {
-            this.nodeNum = nodeNum;
-            this.cost = cost;
-            this.maxCost = maxCost;
-        }
-
         @Override
         public int compareTo(Node o) {
-            return Long.compare(this.maxCost, o.maxCost);
+            return Long.compare(this.cost, o.cost);
         }
     }
 }
