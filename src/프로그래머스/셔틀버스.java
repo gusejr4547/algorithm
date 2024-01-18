4cba605f24bb2abc4cc0a627757f7cc47e0f0ff4
조현덕 <gusejr4547@gmail.com>
2024-01-18 15:52:31 +0900

    셔틀버스

--- src/프로그래머스/셔틀버스.java
+++ src/프로그래머스/셔틀버스.java
@@ -1,6 +1,7 @@
 package 프로그래머스;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class 셔틀버스 {
     public static void main(String[] args) {
@@ -8,60 +9,50 @@ public class 셔틀버스 {
         int n = 10;
         int t = 60;
         int m = 45;
-        String[] timetable = {"23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"};
+        String[] timetable = {"23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"};
         System.out.println(Main.solution(n, t, m, timetable));
     }
 
     public String solution(int n, int t, int m, String[] timetable) {
         String answer = "";
-        int[] timeTable = new int[timetable.length];
-        for (int i = 0; i < timeTable.length; i++) {
-            timeTable[i] = Integer.parseInt(timetable[i].replace(":", ""));
+        PriorityQueue<Integer> waiters = new PriorityQueue<>();
+        for (String time : timetable) {
+            waiters.offer(time2Int(time));
         }
+        int busTime = time2Int("09:00");
+        int busCount = 0;
 
-        // 이진탐색으로 출발 시간 전 태움?
-        int busTime = 900;
-        int index = 0;
-        int last = 0;
-        while (index < timeTable.length) {
-            int i = binarySearch(timeTable, index, timeTable.length - 1, busTime);
-            if (i - index + 1 <= m) {
-                last = i - index + 1;
-                index = i + 1;
-            } else {
-                last = m;
-                index = index + m;
+        while (busCount < n) {
+            if (busCount != 0)
+                busTime += t;
+            int pCount = 0;
+            while (waiters.size() > 0 && pCount < m) {
+                if (waiters.peek() > busTime)
+                    break;
+                if (busCount == n - 1 && pCount == m - 1) {
+                    return answer = int2Time(waiters.poll() - 1);
+                }
+                waiters.poll();
+                pCount++;
             }
-            if (index < timeTable.length)
-                busTime = (busTime + t) % 100 == 60 ? busTime + 40 : busTime + t;
+            busCount++;
         }
 
-        if (last < m) {
-            answer = busTime / 100 + ":" + busTime % 100;
-        }else{
-            busTime = (busTime + t) % 100 == 60 ? busTime + 40 : busTime + t;
-            answer = busTime / 100 + ":" + busTime % 100;
-        }
+        answer = int2Time(busTime);
 
         return answer;
     }
 
-    public int binarySearch(int[] timeTable, int start, int end, int busTime) {
-        int s = start;
-        int e = end;
-        int result = 0;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (timeTable[mid] == busTime) {
-                result = mid;
-                break;
-            } else if (timeTable[mid] < busTime) {
-                s = mid + 1;
-                result = mid;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return result;
+    public int time2Int(String time) {
+        String[] times = time.split(":");
+        int hour = Integer.parseInt(times[0]);
+        int minute = Integer.parseInt(times[1]);
+        return hour * 60 + minute;
+    }
+
+    public String int2Time(int time) {
+        String hour = String.format("%02d", time / 60);
+        String minute = String.format("%02d", time % 60);
+        return hour + ":" + minute;
     }
 }
