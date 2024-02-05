98d468afdc1564f9e20a0051e0b70e047b37185a
조현덕 <gusejr4547@gmail.com>
2024-02-05 14:45:38 +0900

    추석_트래픽

--- src/프로그래머스/추석_트래픽.java
+++ src/프로그래머스/추석_트래픽.java
@@ -7,12 +7,21 @@ import java.util.StringTokenizer;
 public class 추석_트래픽 {
     public static void main(String[] args) {
         추석_트래픽 Main = new 추석_트래픽();
-        String[] lines = {"2016-09-15 01:00:04.001 2.0s",
-                "2016-09-15 01:00:07.000 2s"};
+        String[] lines = {"2016-09-15 20:59:57.421 0.351s",
+                "2016-09-15 20:59:58.233 1.181s",
+                "2016-09-15 20:59:58.299 0.8s",
+                "2016-09-15 20:59:58.688 1.041s",
+                "2016-09-15 20:59:59.591 1.412s",
+                "2016-09-15 21:00:00.464 1.466s",
+                "2016-09-15 21:00:00.741 1.581s",
+                "2016-09-15 21:00:00.748 2.31s",
+                "2016-09-15 21:00:00.966 0.381s",
+                "2016-09-15 21:00:02.066 2.62s"};
         System.out.println(Main.solution(lines));
     }
 
     public int solution(String[] lines) {
+        int len = lines.length;
         List<Task> taskList = new ArrayList<>();
         for (String log : lines) {
             StringTokenizer st = new StringTokenizer(log);
@@ -25,9 +34,21 @@ public class 추석_트래픽 {
             taskList.add(new Task(endTime - processTime + 1, endTime));
         }
 
-        System.out.println(taskList);
-
         int answer = 0;
+        for (int i = 0; i < len; i++) {
+            int cntS = 1;
+            int cntE = 1;
+//            int startTime = taskList.get(i).start;
+            int endTime = taskList.get(i).end;
+            for (int j = i + 1; j < len; j++) {
+                if (endTime + 1000 > taskList.get(j).start) cntE++;
+            }
+
+//            answer = Math.max(answer, cntS);
+            answer = Math.max(answer, cntE);
+        }
+
+
         return answer;
     }
 
