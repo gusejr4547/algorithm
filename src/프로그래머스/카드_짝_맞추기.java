6c0b6474f30ac0aecc68c449fc36c2eb1e68ed78
조현덕 <gusejr4547@gmail.com>
2024-03-08 16:49:46 +0900

    카드_짝_맞추기

--- src/프로그래머스/카드_짝_맞추기.java
+++ src/프로그래머스/카드_짝_맞추기.java
@@ -13,17 +13,19 @@ public class 카드_짝_맞추기 {
 
     boolean[] isNum;
     Point[][] cardPos;
+    int totalCardCnt;
+    int answer;
 
     public int solution(int[][] board, int r, int c) {
+        answer = Integer.MAX_VALUE;
         cardPos = new Point[7][2];
         isNum = new boolean[7];
-        int size = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 if (board[i][j] == 0) continue;
                 if (!isNum[board[i][j]]) {
                     isNum[board[i][j]] = true;
-                    size++;
+                    totalCardCnt++;
                     cardPos[board[i][j]][0] = new Point(i, j);
                 } else {
                     cardPos[board[i][j]][1] = new Point(i, j);
@@ -31,59 +33,72 @@ public class 카드_짝_맞추기 {
             }
         }
 
-        permutation(board, r, c, 0, new int[size], new boolean[size]);
+        permutation(board, r, c, 0, 0);
 
-        int answer = 0;
         return answer;
     }
 
-    int[] dy = {0, 0, 1, -1};
-    int[] dx = {1, -1, 0, 0};
 
     public int bfs(int[][] board, int sy, int sx, int ey, int ex) {
+        int[] dy = {0, 0, 1, -1};
+        int[] dx = {1, -1, 0, 0};
+
         ArrayDeque<State> queue = new ArrayDeque<>();
+
         boolean[][] visit = new boolean[4][4];
-        boolean[][] flip = new boolean[4][4];
-        int idx = 0;
-        queue.offer(new State(y, x, 0));
+        queue.offer(new State(sy, sx, 0));
 
         while (!queue.isEmpty()) {
-            int len = queue.size();
+            State curr = queue.poll();
+            int currY = curr.y;
+            int currX = curr.x;
 
-            for (int i = 0; i < len; i++) {
-                State curr = queue.poll();
-                int currY = curr.y;
-                int currX = curr.x;
-            }
-
+            if (currY == ey && currX == ex)
+                return curr.controlCnt;
 
             if (visit[currY][currX])
                 continue;
             visit[currY][currX] = true;
 
-            // 카드 뒤집기
-            if (board[currY][currX])
+            // 4방향 방향키
+            for (int d = 0; d < 4; d++) {
+                int ny = currY + dy[d];
+                int nx = currX + dx[d];
 
-                // 4방향 방향키
-                for (int d = 0; d < 4; d++) {
-                    int ny = currY + dy[d];
-                    int nx = currX + dx[d];
-
-                    if (ny < 0 || nx < 0 || ny >= 4 || nx >= 4 || visit[ny][nx])
-                        continue;
-
-
-                }
+                if (ny < 0 || nx < 0 || ny >= 4 || nx >= 4 || visit[ny][nx])
+                    continue;
+                queue.offer(new State(ny, nx, curr.controlCnt + 1));
+            }
 
             // 4방향 ctrl+방향키
+            for (int d = 0; d < 4; d++) {
+                int y = currY;
+                int x = currX;
+                while (true) {
+                    y += dy[d];
+                    x += dx[d];
+                    if (y < 0 || x < 0 || y >= 4 || x >= 4) {
+                        y -= dy[d];
+                        x -= dx[d];
+                        break;
+                    }
+                    if (board[y][x] > 0)
+                        break;
+                }
 
-
+                if (visit[y][x])
+                    continue;
+                queue.offer(new State(y, x, curr.controlCnt + 1));
+            }
         }
+
+        return -1;
     }
 
     // 카드 뒤집는 순서 순열
-    public void permutation(int[][] board, int r, int c, int count, int[] numArr) {
-        if (count == numArr.length) {
+    public void permutation(int[][] board, int r, int c, int result, int count) {
+        if (count == totalCardCnt) {
+            answer = Math.min(answer, result);
             return;
         }
 
@@ -92,14 +107,20 @@ public class 카드_짝_맞추기 {
                 continue;
 
             int moveCnt1 = bfs(board, r, c, cardPos[i][0].y, cardPos[i][0].x)
-                    + bfs(board, cardPos[i][0].y, cardPos[i][0].x, cardPos[i][1].y, cardPos[i][1].x);
+                    + bfs(board, cardPos[i][0].y, cardPos[i][0].x, cardPos[i][1].y, cardPos[i][1].x) + 2;
             int moveCnt2 = bfs(board, r, c, cardPos[i][1].y, cardPos[i][1].x)
-                    + bfs(board, cardPos[i][1].y, cardPos[i][1].x, cardPos[i][0].y, cardPos[i][0].x);
+                    + bfs(board, cardPos[i][1].y, cardPos[i][1].x, cardPos[i][0].y, cardPos[i][0].x) + 2;
 
 
             isNum[i] = false;
-            numArr[count] = i;
-            permutation(board, r, c, count + 1, numArr);
+            board[cardPos[i][0].y][cardPos[i][0].x] = 0;
+            board[cardPos[i][1].y][cardPos[i][1].x] = 0;
+            if (moveCnt1 < moveCnt2)
+                permutation(board, cardPos[i][1].y, cardPos[i][1].x, result + moveCnt1, count + 1);
+            else
+                permutation(board, cardPos[i][0].y, cardPos[i][0].x, result + moveCnt2, count + 1);
+            board[cardPos[i][0].y][cardPos[i][0].x] = i;
+            board[cardPos[i][1].y][cardPos[i][1].x] = i;
             isNum[i] = true;
         }
     }
@@ -114,7 +135,7 @@ public class 카드_짝_맞추기 {
         }
     }
 
-    class State implements Comparable<State> {
+    class State {
         int y;
         int x;
         int controlCnt;
@@ -123,11 +144,6 @@ public class 카드_짝_맞추기 {
             this.y = y;
             this.x = x;
             this.controlCnt = controlCnt;
-        }
-
-        @Override
-        public int compareTo(State o) {
-            return this.controlCnt - o.controlCnt;
         }
     }
 }
