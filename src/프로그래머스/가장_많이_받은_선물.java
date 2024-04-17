31d2b4470277acd1fb3aa7c909907f9600202865
조현덕 <gusejr4547@gmail.com>
2024-04-17 14:24:42 +0900

    가장_많이_받은_선물

---
+++ src/프로그래머스/가장_많이_받은_선물.java
@@ -0,0 +1,56 @@
+package 프로그래머스;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 가장_많이_받은_선물 {
+    public static void main(String[] args) {
+        가장_많이_받은_선물 Main = new 가장_많이_받은_선물();
+        String[] friends = {"joy", "brad", "alessandro", "conan", "david"};
+        String[] gifts = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
+        System.out.println(Main.solution(friends, gifts));
+    }
+
+    // A -> B
+    public int solution(String[] friends, String[] gifts) {
+        int friendSize = friends.length;
+        Map<String, Integer> friendIdx = new HashMap<>();
+        int[][] total = new int[friendSize][friendSize];
+        int[] giftPoint = new int[friendSize];
+
+        for (int i = 0; i < friendSize; i++) {
+            friendIdx.put(friends[i], i);
+        }
+
+        for (String gift : gifts) {
+            String[] giftArr = gift.split(" ");
+//            System.out.println(Arrays.toString(giftArr));
+            total[friendIdx.get(giftArr[0])][friendIdx.get(giftArr[1])]++;
+            giftPoint[friendIdx.get(giftArr[0])]++;
+            giftPoint[friendIdx.get(giftArr[1])]--;
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < friendSize; i++) {
+            int receiveCnt = 0;
+            for (int j = 0; j < friendSize; j++) {
+                if (i == j) continue;
+                if ((total[i][j] == 0 && total[j][i] == 0) || total[i][j] == total[j][i]) {
+                    if (giftPoint[i] > giftPoint[j]) {
+                        receiveCnt++;
+                    }
+                } else {
+                    if (total[i][j] > total[j][i]) {
+                        receiveCnt++;
+                    }
+                }
+            }
+            answer = Math.max(answer, receiveCnt);
+        }
+
+
+        return answer;
+    }
+}
