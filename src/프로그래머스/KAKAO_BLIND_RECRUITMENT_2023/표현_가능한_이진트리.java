9d081c08cf071023afedc499034edc8fb99cd768
조현덕 <gusejr4547@gmail.com>
2024-05-01 23:19:15 +0900

    표현_가능한_이진트리

---
+++ src/프로그래머스/KAKAO_BLIND_RECRUITMENT_2023/표현_가능한_이진트리.java
@@ -0,0 +1,56 @@
+package 프로그래머스.KAKAO_BLIND_RECRUITMENT_2023;
+
+import java.util.Arrays;
+
+public class 표현_가능한_이진트리 {
+    public static void main(String[] args) {
+        표현_가능한_이진트리 Main = new 표현_가능한_이진트리();
+        long[] numbers = {7, 42, 5};
+        System.out.println(Arrays.toString(Main.solution(numbers)));
+    }
+
+    public int[] solution(long[] numbers) {
+        int[] answer = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            String binary = num2binary(numbers[i]);
+            // 포화 이진트리 만들기
+            int depth = 1;
+            while (true) {
+                if (binary.length() <= Math.pow(2, depth) - 1) {
+                    binary = String.format("%" + ((int) Math.pow(2, depth) - 1) + "s", binary).replace(' ', '0');
+                    break;
+                }
+                depth++;
+            }
+//            System.out.println(binary);
+            boolean result = search(0, binary.length() - 1, binary);
+            answer[i] = result ? 1 : 0;
+        }
+
+
+        return answer;
+    }
+
+    private boolean search(int left, int right, String binary) {
+        if (left == right) {
+            return true;
+        }
+
+        int mid = (left + right) / 2;
+        if (binary.charAt(mid) == '0') {
+            return false;
+        }
+        // 왼쪽 오른쪽
+        return search(left, mid - 1, binary) && search(mid + 1, right, binary);
+    }
+
+    private String num2binary(long num) {
+        StringBuilder result = new StringBuilder();
+        while (num > 0) {
+            result.insert(0, num % 2);
+            num /= 2;
+        }
+        return result.toString();
+    }
+}
