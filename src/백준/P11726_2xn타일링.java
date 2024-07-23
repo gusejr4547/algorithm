bd9e6d4c283eb0f02ade968ac1f52ee44372c07a
조현덕 <gusejr4547@gmail.com>
2024-07-23 13:12:38 +0900

    P11726_2xn타일링

---
+++ src/백준/P11726_2xn타일링.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P11726_2xn타일링 {
+    static final int MOD = 10007;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
