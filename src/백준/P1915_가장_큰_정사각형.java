e6f4dbb0b8ff993c67ce7fde579435040b29de24
조현덕 <gusejr4547@gmail.com>
2024-07-26 16:41:51 +0900

    P1915_가장_큰_정사각형 : DP

--- src/백준/P1915_가장_큰_정사각형.java
+++ src/백준/P1915_가장_큰_정사각형.java
@@ -7,7 +7,32 @@ import java.util.StringTokenizer;
 
 public class P1915_가장_큰_정사각형 {
     public static void main(String[] args) throws Exception {
-        prefixSolution();
+//        prefixSolution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][m + 1];
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            char[] carr = br.readLine().toCharArray();
+            for (int j = 1; j <= m; j++) {
+                int val = carr[j - 1] - '0';
+                if (i == 1 && j == 1) {
+                    dp[i][j] = val;
+                    max = dp[i][j];
+                } else {
+                    if (val == 1) {
+                        int up = dp[i - 1][j];
+                        int left = dp[i][j - 1];
+                        int ul = dp[i - 1][j - 1];
+                        dp[i][j] = Math.min(up, Math.min(left, ul)) + 1;
+                        max = Math.max(max, dp[i][j]);
+                    }
+                }
+            }
+        }
+        System.out.println(max * max);
     }
 
     private static void prefixSolution() throws Exception {
