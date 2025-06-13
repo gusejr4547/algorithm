425ff135677a12b323f1222084cd72848f569fe8
조현덕 <gusejr4547@gmail.com>
2025-06-13 14:07:20 +0900

    P15824_너_봄에는_캡사이신이_맛있단다

---
+++ src/백준/P15824_너_봄에는_캡사이신이_맛있단다.java
@@ -0,0 +1,47 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P15824_너_봄에는_캡사이신이_맛있단다 {
+    static int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] menu = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            menu[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(menu);
+        long answer = 0;
+
+        // 최댓값으로 쓰이는 경우 전부 +
+        // 최솟값으로 쓰이는 경우 전부 -
+        for (int i = 0; i < N; i++) {
+            long max = menu[i] * powerMod(2, i, MOD);
+            long min = menu[i] * powerMod(2, N - 1 - i, MOD);
+
+            answer += max - min % MOD;
+        }
+
+        System.out.println(answer);
+    }
+
+    private static long powerMod(int base, int exp, int mod) {
+        if (exp == 0) {
+            return 1;
+        }
+        long next = powerMod(base, exp / 2, mod);
+        if (exp % 2 == 1) {
+            return next * next * base % mod;
+        } else {
+            return next * next % mod;
+        }
+    }
+}
