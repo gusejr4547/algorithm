88bf214324508b523fd489943ec65636eb972948
조현덕 <gusejr4547@gmail.com>
2025-06-24 13:41:19 +0900

    서로_다른_BST_개수_세기

--- src/코드트리/서로_다른_BST_개수_세기.java
+++ src/코드트리/서로_다른_BST_개수_세기.java
@@ -8,11 +8,12 @@ public class 서로_다른_BST_개수_세기 {
         int n = sc.nextInt();
         // Please write your code here.
         int[] dp = new int[20];
+        dp[0] = 1;
         dp[1] = 1;
 
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1]*2;
-            for(int j=1; j<i-1; j++){
+            // dp[i] = dp[i-1]*2;
+            for(int j=0; j<i; j++){
                 dp[i] += dp[j] * dp[i-1-j];
             }
         }
