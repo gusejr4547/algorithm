2a6b43bad17deae44caed52ba6f4c20e5a51be76
조현덕 <gusejr4547@gmail.com>
2025-06-26 19:12:59 +0900

    배낭_채우기

--- src/코드트리/배낭_채우기.java
+++ src/코드트리/배낭_채우기.java
@@ -14,21 +14,18 @@ public class 배낭_채우기 {
             v[i] = sc.nextInt();
         }
         // Please write your code here.
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[m+1];
         for(int i=1; i<=n; i++){
             int weight = w[i];
             int value = v[i];
-            for(int j=1; j<=m; j++){
-                if(j < weight){
-                    dp[i][j] = dp[i-1][j];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight] + value);
-                }
+            for(int j=m; j>=weight; j--){
+                dp[j] = Math.max(dp[j], dp[j-weight] + value);
             }
         }
         // for(int[] a : dp){
         //     System.out.println(Arrays.toString(a));
         // }
-        System.out.println(dp[n][m]);
+        // System.out.println(Arrays.toString(dp));
+        System.out.println(dp[m]);
     }
 }
