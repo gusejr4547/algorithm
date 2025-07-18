8a0f1e16a4cba39fffe28b3569922a6422e67ba6
조현덕 <gusejr4547@gmail.com>
2025-07-18 14:48:02 +0900

    P11724_연결_요소의_개수

---
+++ src/백준/P11724_연결_요소의_개수.java
@@ -0,0 +1,56 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class P11724_연결_요소의_개수 {
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            union(u, v);
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 1; i <= N; i++) {
+            set.add(find(i));
+        }
+
+        System.out.println(set.size());
+    }
+
+    private static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (y > x) {
+            parent[y] = x;
+        } else if (x > y) {
+            parent[x] = y;
+        }
+    }
+}
