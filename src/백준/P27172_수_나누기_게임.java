5b6548e2c4516ff246325024c88acd907f96c38d
조현덕 <gusejr4547@gmail.com>
2025-07-23 15:01:03 +0900

    P27172_수_나누기_게임

--- src/백준/P27172_수_나누기_게임.java
+++ src/백준/P27172_수_나누기_게임.java
@@ -15,45 +15,57 @@ public class P27172_수_나누기_게임 {
         card = new int[N];
         Map<Integer, List<Integer>> divisors = new HashMap<>();
         Map<Integer, Integer> numToIdx = new HashMap<>();
+
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
             card[i] = Integer.parseInt(st.nextToken());
             divisors.put(card[i], new ArrayList<>());
             numToIdx.put(card[i], i);
+            max = Math.max(max, card[i]);
         }
 
         score = new int[N];
 
-        // 안됨
+        // 에라스토테네스 체
+        int[] pos = new int[max + 1];
+        Arrays.fill(pos, -1);
+        for (int i = 0; i < N; i++) {
+            pos[card[i]] = i;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int num = card[i] * 2; num <= max; num += card[i]) {
+                if (pos[num] != -1) {
+                    score[i]++;
+                    score[pos[num]]--;
+                }
+            }
+        }
+
+
+        // 약수
 //        for (int i = 0; i < N; i++) {
-//            for (int j = i + 1; j < N; j++) {
-//                // i와 j가 대결
+//            for (int num = 1; num * num <= card[i]; num++) {
+//                if (card[i] % num == 0) {
+//                    divisors.get(card[i]).add(num);
+//                    if (card[i] / num != num) {
+//                        divisors.get(card[i]).add(card[i] / num);
+//                    }
+//                }
+//            }
+//        }
 //
+//        for (int i = 0; i < N; i++) {
+//            for (int div : divisors.get(card[i])) {
+//                if (numToIdx.containsKey(div)) {
+//                    score[i]--;
+//                    score[numToIdx.get(div)]++;
+//                }
 //            }
 //        }
 
-        // 약수
-        for (int i = 0; i < N; i++) {
-            for (int num = 1; num * num <= card[i]; num++) {
-                if (card[i] % num == 0) {
-                    divisors.get(card[i]).add(num);
-                    if (card[i] / num != num) {
-                        divisors.get(card[i]).add(card[i] / num);
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int div : divisors.get(card[i])) {
-                if (numToIdx.containsKey(div)) {
-                    score[i]--;
-                    score[numToIdx.get(div)]++;
-                }
-            }
-        }
-
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             sb.append(score[i]).append(" ");
         }
 
