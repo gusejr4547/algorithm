5e3f51b992c78694fdfc58825842fb07b48a7400
조현덕 <gusejr4547@gmail.com>
2025-08-03 20:38:42 +0900

    P9655_돌_게임3

---
+++ src/백준/P9657_돌_게임3.java
@@ -0,0 +1,25 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P9657_돌_게임3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean[] win = new boolean[N + 1];
+
+        win[1] = true;
+        win[3] = true;
+        win[4] = true;
+
+        for (int i = 5; i <= N; i++) {
+            if (!win[i - 4] || !win[i - 3] || !win[i - 1]) {
+                win[i] = true;
+            }
+        }
+//        System.out.println(Arrays.toString(win));
+        System.out.println(win[N] ? "SK" : "CY");
+    }
+}
