cda9292e4e5930311c18eb0ad06395981961efca
조현덕 <gusejr4547@gmail.com>
2025-09-19 16:19:09 +0900

    순열로_고치기

--- src/NYPC/기출2025/연습문제/순열로_고치기.java
+++ src/NYPC/기출2025/연습문제/순열로_고치기.java
@@ -13,30 +13,46 @@ public class 순열로_고치기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] seq = new int[N];
-        for (int i = 0; i < N; i++) {
+        int[] seq = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            m.put(seq[i], m.getOrDefault(seq[i], 0) + 1);
-        }
-        System.out.println(m);
-
-        long answer = 0;
-        // N 넘어가는거 무조건 바꿔야함.
-        // 중복되는거 있으면 앞에 나온거를 바꿔야함.
-        for (int i = 0; i < N; i++) {
-            if (seq[i] > N) {
-                answer += (i + 1);
-            } else {
-                if (m.get(seq[i]) > 1) {
-                    m.put(seq[i], m.get(seq[i]) - 1);
-                    answer += (i + 1);
-                }
+        int[] maxIdx = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            if (seq[i] <= N) {
+                maxIdx[seq[i]] = i;
             }
         }
+
+        long answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (seq[i] > N || maxIdx[seq[i]] != i) {
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+
+
+//        Map<Integer, Integer> m = new HashMap<>();
+//        for (int i = 0; i < N; i++) {
+//            m.put(seq[i], m.getOrDefault(seq[i], 0) + 1);
+//        }
+////        System.out.println(m);
+//
+//        long answer = 0;
+//        // N 넘어가는거 무조건 바꿔야함.
+//        // 중복되는거 있으면 앞에 나온거를 바꿔야함.
+//        for (int i = 0; i < N; i++) {
+//            if (seq[i] > N) {
+//                answer += (i + 1);
+//            } else {
+//                if (m.get(seq[i]) > 1) {
+//                    m.put(seq[i], m.get(seq[i]) - 1);
+//                    answer += (i + 1);
+//                }
+//            }
+//        }
 
         System.out.println(answer);
     }
