0a128f81de5d3fbc4a82d1d110b322a74b1879a0
조현덕 <gusejr4547@gmail.com>
2025-09-26 14:32:56 +0900

    점_찍기

--- src/프로그래머스/점_찍기.java
+++ src/프로그래머스/점_찍기.java
@@ -10,31 +10,14 @@ public class 점_찍기 {
 
     }
 
-    public long solution(int k, int d) {
+    public long solution(int k, long d) {
         long answer = 0;
 
-        // y축에 찍히는 점 최대
-        int y = 0;
-        while (y <= d) {
-            y += k;
-        }
-        y -= k;
-
-        // x 위치를 옮겨 가며 최대 y 값을 조정
-        for (int x = 0; x <= d; x += k) {
-            while (y >= 0) {
-                if (Math.ceil(getDistance(x, y)) <= d) {
-                    answer += y / k + 1;
-                    break;
-                }
-                y -= k;
-            }
+        for (long x = 0; x <= d; x += k) {
+            long maxY = (long) Math.sqrt(d * d - x * x);
+            answer += maxY/k + 1;
         }
 
         return answer;
-    }
-
-    private static double getDistance(long a, long b) {
-        return Math.sqrt(a * a + b * b);
     }
 }
