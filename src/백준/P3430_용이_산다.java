ad97e9fc8998d3707f717ad4ec41b488e92c3f14
조현덕 <gusejr4547@gmail.com>
2025-10-02 15:19:56 +0900

    P3430_용이_산다

--- src/백준/P3430_용이_산다.java
+++ src/백준/P3430_용이_산다.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 비가 내렸을 때 그 호수가 비어있다면 그 호수는 다시 물로 꽉 차지만,
+// 만약 호수에 물이 있는데 다시 비가 내릴경우 호수가 넘쳐 흘러서 엄청난 재앙
+// 비가 안오는날 1개의 호수 비울 수 있다.
 public class P3430_용이_산다 {
     static StringBuilder sb = new StringBuilder();
+    static TreeSet<Integer> drinkTime = new TreeSet<>();
+    static int[] rainTime = new int[1_000_000], result = new int[1_000_000];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,61 +32,45 @@ public class P3430_용이_산다 {
     }
 
     static void solution(int n, int m, int[] t) {
-        boolean[] fill = new boolean[n + 1];
-        int[] next = new int[m];
-        int[] prev = new int[n + 1];
-        boolean flood = false;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.naturalOrder());
-        ArrayDeque<Integer> result = new ArrayDeque<>();
-
-        Arrays.fill(fill, true);
-
-        // 각 호수에 비가오는 가장 빠른 시간을 pq에 추가
-        for (int i = 0; i < m; i++) {
-            int lakeNum = t[i];
-            if (lakeNum > 0) {
-                if (prev[lakeNum] > 0) {
-                    next[prev[lakeNum]] = i;
-                } else {
-                    pq.offer(i);
-                }
-                prev[lakeNum] = i;
-            }
-        }
+        // n, m <= 1_000_000
+        // 이전에 비가 내렸던 시간에 가장 가까운 물 마실 수 있는 시간을 사용.
+        boolean isFlood = false;
+        drinkTime.clear(); // 비가 안내린 시간
+        Arrays.fill(rainTime, -1);
+        Arrays.fill(result, 0);
 
         for (int i = 0; i < m; i++) {
-            int lakeNum = t[i];
-            if (lakeNum > 0) {
-                if (fill[lakeNum]) {
-                    flood = true;
-                    break;
-                } else {
-                    fill[lakeNum] = true;
-                    if (next[i] > 0) {
-                        pq.offer(next[i]);
-                    }
-                }
+            if (t[i] == 0) {
+                drinkTime.add(i);
             } else {
-                if (!pq.isEmpty()) {
-                    int selectDay = pq.poll();
-                    int select = t[selectDay];
-                    fill[select] = false;
-                    result.offer(select);
+                // 내린 곳 한번 더 내림 => 물 마시기
+                if (!drinkTime.isEmpty()) {
+                    Integer drink = drinkTime.higher(rainTime[t[i]]);
+                    if (drink != null) {
+                        result[drink] = t[i];
+                        drinkTime.remove(drink);
+                        rainTime[t[i]] = i;
+                    } else {
+                        isFlood = true;
+                        break;
+                    }
                 } else {
-                    result.offer(0);
+                    isFlood = true;
+                    break;
                 }
             }
         }
 
-
-        if (!flood) {
-            sb.append("YES\n");
-            while(!result.isEmpty()){
-                sb.append(result.poll()).append(" ");
-            }
-            sb.append("\n");
+        if (isFlood) {
+            sb.append("NO");
         } else {
-            sb.append("NO\n");
+            sb.append("YES").append("\n");
+            for (int i = 0; i < m; i++) {
+                if (t[i] == 0) {
+                    sb.append(result[i]).append(" ");
+                }
+            }
         }
+        sb.append("\n");
     }
 }
