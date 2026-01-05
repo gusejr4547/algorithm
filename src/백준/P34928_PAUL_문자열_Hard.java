8d29cd6f0dc154e4fd5b141e45bd0b5211b2acc7
조현덕 <gusejr4547@gmail.com>
2026-01-05 17:05:05 +0900

    P34928_PAUL_문자열_Hard

--- src/백준/P34928_PAUL_문자열_Hard.java
+++ src/백준/P34928_PAUL_문자열_Hard.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class P34928_PAUL_문자열_Hard {
     static int N;
     static String str;
-    static List<Integer>[] idxList;
     static boolean answer;
 
     public static void main(String[] args) throws Exception {
@@ -16,112 +15,21 @@ public class P34928_PAUL_문자열_Hard {
         N = Integer.parseInt(br.readLine());
         str = br.readLine();
 
-        idxList = new List[4];
-        for (int i = 0; i < 4; i++) {
-            idxList[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < N; i++) {
-            if ('P' == str.charAt(i)) {
-                idxList[0].add(i);
-            } else if ('A' == str.charAt(i)) {
-                idxList[1].add(i);
-            } else if ('U' == str.charAt(i)) {
-                idxList[2].add(i);
-            } else if ('L' == str.charAt(i)) {
-                idxList[3].add(i);
-            }
-        }
-
-        // P A L U 조합...
-        // P 가능?
-        boolean[] canMake1 = new boolean[idxList[0].size()];
-        for (int i = 0; i < idxList[0].size(); i++) {
-            if (idxList[0].get(i) % 2 == 0) {
-                canMake1[i] = true;
-            }
-        }
-        boolean[] canMake2 = new boolean[idxList[1].size()];
-        for (int i = 0; i < idxList[1].size(); i++) {
-            int cur = idxList[1].get(i);
-            for (int j = 0; j < idxList[0].size(); j++) {
-                int prev = idxList[0].get(j);
-                if (canMake1[j] && prev < cur) {
-                    if ((cur - prev - 1) % 2 == 0) {
-                        canMake2[i] = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        boolean[] canMake3 = new boolean[idxList[2].size()];
-        for (int i = 0; i < idxList[2].size(); i++) {
-            int cur = idxList[2].get(i);
-            for (int j = 0; j < idxList[1].size(); j++) {
-                int prev = idxList[1].get(j);
-                if (canMake2[j] && prev < cur) {
-                    if ((cur - prev - 1) % 2 == 0) {
-                        canMake3[i] = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        boolean[] canMake4 = new boolean[idxList[3].size()];
-        for (int i = 0; i < idxList[3].size(); i++) {
-            int cur = idxList[3].get(i);
-            for (int j = 0; j < idxList[2].size(); j++) {
-                int prev = idxList[2].get(j);
-                if (canMake3[j] && prev < cur) {
-                    if ((cur - prev - 1) % 2 == 0) {
-                        canMake4[i] = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < idxList[3].size(); i++) {
-            if (canMake4[i] && (N - idxList[3].get(i) - 1) % 2 == 0) {
-                answer = true;
-                break;
-            }
-        }
-
-        System.out.println(answer ? "YES" : "NO");
-    }
-
-    private static void dfs(int PAULIdx, int prev) {
-        // 조기 종료
-        if (answer) {
+        // 길이를 2씩 줄여서 4(PAUL)로 만들어야 하므로, N은 짝수여야 함
+        if (N % 2 != 0) {
+            System.out.println("NO");
             return;
         }
 
-        // 마지막
-        if (PAULIdx == 4) {
-            // 거리가 2의 배수?
-            if ((N - prev) % 2 == 0) {
-                answer = true;
-            }
-            return;
-        }
-
-        for (int idx : idxList[PAULIdx]) {
-            if (prev > idx) {
-                continue;
-            }
-
-            // 거리가 2의 배수?
-            if ((idx - prev) % 2 == 0) {
-                dfs(PAULIdx + 1, idx + 1);
-            }
-
-            // 조기 종료
-            if (answer) {
-                return;
+        String PAUL = "PAUL";
+        int idx = 0;
+        // 원본 문자열에서 짝수 인덱스의 P, 홀수 인덱스의 A, 짝수 인덱스의 U, 홀수 인덱스의 L을 순서대로 찾을 수 있다면 가능
+        for (int i = 0; i < N && idx < 4; i++) {
+            if (PAUL.charAt(idx) == str.charAt(i) && i % 2 == idx % 2) {
+                idx++;
             }
         }
+
+        System.out.println(idx == 4 ? "YES" : "NO");
     }
 }
