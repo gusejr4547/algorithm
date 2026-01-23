b8ba36101b68ccf39467edea6b85f75c36c8cfc6
조현덕 <gusejr4547@gmail.com>
2026-01-23 21:42:12 +0900

    P16958_텔레포트

--- src/백준/P16958_텔레포트.java
+++ src/백준/P16958_텔레포트.java
@@ -22,28 +22,44 @@ public class P16958_텔레포트 {
             cities[i] = new City(s, x, y);
         }
 
-        int[][] dist = new int[N + 1][N + 1];
+        // 가장 가까운 텔레포트 도시
+        int[] nearTp = new int[N + 1];
+        Arrays.fill(nearTp, 999_999_999);
+
         for (int i = 1; i <= N; i++) {
+            if (cities[i].s == 1) {
+                nearTp[i] = 0;
+            }
+
             for (int j = 1; j <= N; j++) {
-                if (i == j) {
-                    dist[i][j] = 0;
-                    continue;
-                }
-                dist[i][j] = getDist(cities[i].x, cities[i].y, cities[j].x, cities[j].y);
-                if (cities[i].s == 1 && cities[j].s == 1) {
-                    dist[i][j] = Math.min(dist[i][j], T);
+                if (cities[j].s == 1) {
+                    nearTp[i] = Math.min(nearTp[i], getDist(cities[i].x, cities[i].y, cities[j].x, cities[j].y));
                 }
             }
         }
 
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    // i에서 j로 가는 거리 vs k를 중간에 경유하는 거리
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
-        }
+//        int[][] dist = new int[N + 1][N + 1];
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= N; j++) {
+//                if (i == j) {
+//                    dist[i][j] = 0;
+//                    continue;
+//                }
+//                dist[i][j] = getDist(cities[i].x, cities[i].y, cities[j].x, cities[j].y);
+//                if (cities[i].s == 1 && cities[j].s == 1) {
+//                    dist[i][j] = Math.min(dist[i][j], T);
+//                }
+//            }
+//        }
+//
+//        for (int k = 1; k <= N; k++) {
+//            for (int i = 1; i <= N; i++) {
+//                for (int j = 1; j <= N; j++) {
+//                    // i에서 j로 가는 거리 vs k를 중간에 경유하는 거리
+//                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+//                }
+//            }
+//        }
 
         StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(br.readLine());
@@ -52,7 +68,13 @@ public class P16958_텔레포트 {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            sb.append(dist[A][B]).append('\n');
+//            sb.append(dist[A][B]).append('\n');
+            int walk = getDist(cities[A].x, cities[A].y, cities[B].x, cities[B].y);
+
+            // 텔레포트
+            int tp = nearTp[A] + T + nearTp[B];
+
+            sb.append(Math.min(walk, tp)).append('\n');
         }
 
         System.out.println(sb);
