3d3cc03b06c950aa3c5b8255ac8d5ccd04c758d8
조현덕 <gusejr4547@gmail.com>
2026-02-02 14:35:17 +0900

    P15661_링크와_스타트

---
+++ src/백준/P15661_링크와_스타트.java
@@ -0,0 +1,53 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15661_링크와_스타트 {
+    // 총 N명
+    // 두 팀으로 나눈다. 팀에는 적어도 1명이상
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] S = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                S[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        // N개의 bit, 0 1로 팀을 구분
+        for (int team = 1; team < (1 << N) - 1; team++) {
+            // 1
+            int teamA = 0;
+            for (int i = 0; i < N; i++) {
+                if ((team & (1 << i)) != 0) {
+                    for (int j = i + 1; j < N; j++) {
+                        if ((team & (1 << j)) != 0) {
+                            teamA += S[i][j] + S[j][i];
+                        }
+                    }
+                }
+            }
+
+            // 0
+            int teamB = 0;
+            for (int i = 0; i < N; i++) {
+                if ((team & (1 << i)) == 0) {
+                    for (int j = i + 1; j < N; j++) {
+                        if ((team & (1 << j)) == 0) {
+                            teamB += S[i][j] + S[j][i];
+                        }
+                    }
+                }
+            }
+
+            answer = Math.min(answer, Math.abs(teamA - teamB));
+        }
+        System.out.println(answer);
+    }
+}
