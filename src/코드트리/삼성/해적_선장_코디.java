73f671ddc890ca0bf7b43cc0e0d69e0f1753bf9f
조현덕 <gusejr4547@gmail.com>
2026-02-03 16:36:38 +0900

    해적_선장_코디

---
+++ src/코드트리/삼성/해적_선장_코디.java
@@ -0,0 +1,134 @@
+package 코드트리.삼성;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 해적_선장_코디 {
+    static List<Ship> shipList;
+    static Map<Integer, Integer> id2Idx, changed;
+    static PriorityQueue<Ship> ready, reload;
+    static Set<Integer> reloadSet;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        shipList = new ArrayList<>();
+        id2Idx = new HashMap<>();
+        changed = new HashMap<>();
+        ready = new PriorityQueue<>((o1, o2) ->
+                o1.p == o2.p ? Integer.compare(o1.id, o2.id) : Integer.compare(o2.p, o1.p));
+        reload = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.reloadEnd, o2.reloadEnd));
+        reloadSet = new HashSet<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            // 공격 준비
+            if (100 == op) {
+                int N = Integer.parseInt(st.nextToken());
+                for (int i = 0; i < N; i++) {
+                    int id = Integer.parseInt(st.nextToken());
+                    int p = Integer.parseInt(st.nextToken());
+                    int r = Integer.parseInt(st.nextToken());
+                    addShip(id, p, r);
+                }
+            }
+            // 지원 요청
+            else if (200 == op) {
+                int id = Integer.parseInt(st.nextToken());
+                int p = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                addShip(id, p, r);
+            }
+            // 함포 교체
+            else if (300 == op) {
+                int id = Integer.parseInt(st.nextToken());
+                int pw = Integer.parseInt(st.nextToken());
+                change(id, pw);
+            }
+            // 공격 명령
+            else if (400 == op) {
+                sb.append(attack(t)).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static String attack(int time) {
+        // reload에서 현재 시간 이전꺼 전부 뽑아서 ready에
+        // p 갱신 필요한거는 바꿔서 넣기
+        while (!reload.isEmpty() && reload.peek().reloadEnd <= time) {
+            Ship s = reload.poll();
+            if (changed.containsKey(s.id)) {
+                if (changed.get(s.id) != s.p) {
+                    ready.offer(new Ship(s.id, changed.get(s.id), s.r));
+                } else {
+                    ready.offer(s);
+                }
+            } else {
+                ready.offer(s);
+            }
+            reloadSet.remove(s.id);
+        }
+
+        // ready에서 최대 5개 뽑기
+        // 뽑은게 reload 중인지 확인
+        // 뽑은게 p가 현재 상태와 같은지 확인
+        // 사격 후 reload에
+        int sum = 0;
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (!ready.isEmpty() && cnt < 5) {
+            Ship s = ready.poll();
+            if (reloadSet.contains(s.id)) {
+                continue;
+            }
+            if (changed.containsKey(s.id) && changed.get(s.id) != s.p) {
+                continue;
+            }
+
+            reload.offer(new Ship(s.id, s.p, s.r, time + s.r));
+            reloadSet.add(s.id);
+
+            sum += s.p;
+            cnt++;
+            sb.append(s.id).append(' ');
+        }
+
+        return sum + " " + cnt + " " + sb.toString();
+    }
+
+    private static void change(int id, int pw) {
+        int idx = id2Idx.get(id);
+        shipList.get(idx).p = pw;
+        changed.put(id, pw);
+        ready.offer(new Ship(id, pw, shipList.get(idx).r));
+    }
+
+    private static void addShip(int id, int p, int r) {
+        shipList.add(new Ship(id, p, r));
+        id2Idx.put(id, shipList.size() - 1);
+        ready.add(new Ship(id, p, r));
+    }
+
+    private static class Ship {
+        int id, p, r, reloadEnd;
+
+        public Ship(int id, int p, int r) {
+            this.id = id;
+            this.p = p;
+            this.r = r;
+        }
+
+        public Ship(int id, int p, int r, int reloadEnd) {
+            this.id = id;
+            this.p = p;
+            this.r = r;
+            this.reloadEnd = reloadEnd;
+        }
+    }
+}
