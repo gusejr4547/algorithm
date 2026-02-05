3fed3440d29d9f1fe8b9a601f699158a0c170f77
조현덕 <gusejr4547@gmail.com>
2026-02-05 16:16:00 +0900

    가로등_설치

--- src/코드트리/삼성/가로등_설치.java
+++ src/코드트리/삼성/가로등_설치.java
@@ -12,20 +12,20 @@ public class 가로등_설치 {
     // N <= 1_000_000_000
 
     static int N;
-    static List<Light> lightList;
+    static List<Integer> posId;
     static TreeMap<Integer, Integer> lightMap;
     static PriorityQueue<Distance> pq;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        lightList = new ArrayList<>();
-        lightList.add(new Light(0, 0)); // 더미 데이터
+        posId = new ArrayList<>();
+        posId.add(0); // 더미 데이터
         lightMap = new TreeMap<>();
         // 거리 내림차순
         pq = new PriorityQueue<>((o1, o2) ->
-                o1.dist == o2.dist ? Integer.compare(lightList.get(o1.leftId).x, lightList.get(o2.leftId).x) :
-                        Integer.compare(o2.dist, o1.dist));
+                o1.dist != o2.dist ? Integer.compare(o2.dist, o1.dist) : Integer.compare(o1.leftX, o2.leftX));
         StringBuilder sb = new StringBuilder();
+
         int Q = Integer.parseInt(br.readLine());
         while (Q-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -34,14 +34,18 @@ public class 가로등_설치 {
                 // 마을 상태 확인
                 N = Integer.parseInt(st.nextToken());
                 int M = Integer.parseInt(st.nextToken());
-                lightList.add(new Light(1, Integer.parseInt(st.nextToken())));
-                lightMap.put(lightList.get(1).x, 1);
-                for (int i = 2; i <= M; i++) {
-                    lightList.add(new Light(i, Integer.parseInt(st.nextToken())));
-                    // 가로등 사이 거리 저장
-                    pq.offer(new Distance(i - 1, i, lightList.get(i).x - lightList.get(i - 1).x));
+
+                int prevX = -1;
+                for (int i = 1; i <= M; i++) {
+                    int x = Integer.parseInt(st.nextToken());
+                    posId.add(x);
                     // lightMap 가로등 위치 순서대로
-                    lightMap.put(lightList.get(i).x, i);
+                    lightMap.put(x, i);
+                    // 가로등 사이 거리 저장
+                    if (prevX != -1) {
+                        pq.offer(new Distance(prevX, x, x - prevX));
+                    }
+                    prevX = x;
                 }
 
             } else if (200 == op) {
@@ -67,9 +71,14 @@ public class 가로등_설치 {
         // 가장 먼 거리의 절반만큼 power가 필요하다.
         while (!pq.isEmpty()) {
             Distance d = pq.peek();
-            if (!lightList.get(d.leftId).isRemoved && !lightList.get(d.rightId).isRemoved) {
-                res = d.dist;
-                break;
+            // leftX, rightX lightMap에 존재해야 유효
+            if (lightMap.containsKey(d.leftX) && lightMap.containsKey(d.rightX)) {
+                // leftX 다음 rightX 가 있어야함.
+                Integer next = lightMap.higherKey(d.leftX);
+                if (next != null && next == d.rightX) {
+                    res = d.dist;
+                    break;
+                }
             }
             pq.poll();
         }
@@ -83,18 +92,18 @@ public class 가로등_설치 {
     }
 
     private static void removeLight(int id) {
-        Light l = lightList.get(id);
+        int x = posId.get(id); // id에 해당되는 x
         // id의 left, right를 찾아서 pq에 Distance 추가
-        Integer rightX = lightMap.higherKey(l.x);
-        Integer leftX = lightMap.lowerKey(l.x);
+        Integer rightX = lightMap.higherKey(x);
+        Integer leftX = lightMap.lowerKey(x);
         // left, right 둘중 하나라도 없으면 Distance 만들기x
         if (rightX != null && leftX != null) {
-            pq.offer(new Distance(lightMap.get(leftX), lightMap.get(rightX), rightX - leftX));
+            pq.offer(new Distance(leftX, rightX, rightX - leftX));
         }
 
         // id번 가로등을 제거한다.
-        lightList.get(id).isRemoved = true;
-        lightMap.remove(lightList.get(id).x);
+        posId.set(id, -1); // -1이 제거를 의미
+        lightMap.remove(x);
     }
 
     private static void addLight() {
@@ -103,11 +112,15 @@ public class 가로등_설치 {
         // i, j 좌표의 경우. 2로 나누어떨어지면 (i+j)/2, 아니면 ceil((i+j)/2)
 
         // pq에서 가장 앞에 있는거
-        // Distance에서 left, right 가로등이 전부 유효해야한다.
         while (!pq.isEmpty()) {
             Distance d = pq.peek();
-            if (!lightList.get(d.leftId).isRemoved && !lightList.get(d.rightId).isRemoved) {
-                break;
+            // leftX, rightX lightMap에 존재해야 유효
+            if (lightMap.containsKey(d.leftX) && lightMap.containsKey(d.rightX)) {
+                // leftX 다음 rightX 가 있어야함.
+                Integer next = lightMap.higherKey(d.leftX);
+                if (next != null && next == d.rightX) {
+                    break;
+                }
             }
             pq.poll();
         }
@@ -118,23 +131,23 @@ public class 가로등_설치 {
         // pq 뽑기
         Distance d = pq.poll();
         // 중간에 가로등 설치
-        int x = (int) Math.ceil((lightList.get(d.rightId).x + lightList.get(d.leftId).x) / 2.0);
-        int id = lightList.size();
-        lightList.add(new Light(id, x));
+        int x = (int) Math.ceil((d.leftX + d.rightX) / 2.0);
+        int id = posId.size();
+        posId.add(x);
         lightMap.put(x, id);
         // 가로등 거리 갱신
         // left - x
-        pq.offer(new Distance(d.leftId, id, x - lightList.get(d.leftId).x));
+        pq.offer(new Distance(d.leftX, x, x - d.leftX));
         // x - right
-        pq.offer(new Distance(id, d.rightId, lightList.get(d.rightId).x - x));
+        pq.offer(new Distance(x, d.rightX, d.rightX - x));
     }
 
     private static class Distance {
-        int leftId, rightId, dist;
+        int leftX, rightX, dist;
 
-        public Distance(int leftId, int rightId, int dist) {
-            this.leftId = leftId;
-            this.rightId = rightId;
+        public Distance(int leftX, int rightX, int dist) {
+            this.leftX = leftX;
+            this.rightX = rightX;
             this.dist = dist;
         }
     }
