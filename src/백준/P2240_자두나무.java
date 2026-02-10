18c533e5c02bdae763c49c63a0d9dc72a1046173
조현덕 <gusejr4547@gmail.com>
2026-02-10 13:46:19 +0900

    P2240_자두나무 최적화

--- src/백준/P2240_자두나무.java
+++ src/백준/P2240_자두나무.java
@@ -19,41 +19,23 @@ public class P2240_자두나무 {
             drop[i] = Integer.parseInt(br.readLine());
         }
 
-        int[][][] dp = new int[T + 1][W + 1][2]; // dp[i][j][k] i초에 j번 위치변경 k에 위치해있을때 최대개수
-        for (int i = 0; i <= T; i++) {
-            for (int j = 0; j <= W; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
-        }
-        // 처음은 0에 위치
-        dp[0][0][0] = 0;
+        int[][] dp = new int[T + 1][W + 1]; // dp[i][j][k] i초에 j번 위치변경 최대개수, j가 짝수이면 1 홀수이면 2
 
         for (int i = 1; i <= T; i++) {
-            int p = drop[i] - 1; // p번에 떨어짐
-            for (int j = 0; j <= W; j++) {
-                // 0
-                if (j != 0) {
-                    dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j - 1][1]);
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0];
-                }
+            int p = drop[i]; // p번에 떨어짐
+            // 0번 움직인 경우는 계속 1번에 있는것이다.
+            dp[i][0] = dp[i - 1][0] + (p == 1 ? 1 : 0);
 
-                // 1
-                if (j != 0) {
-                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0]);
-                } else {
-                    dp[i][j][1] = dp[i - 1][j][1];
-                }
+            for (int j = 1; j <= W; j++) {
+                int my = j % 2 == 0 ? 1 : 2;
 
-                if (dp[i][j][p] != -1) {
-                    dp[i][j][p] += 1;
-                }
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + (p == my ? 1 : 0);
             }
         }
 
         int answer = 0;
         for (int i = 0; i <= W; i++) {
-            answer = Math.max(answer, Math.max(dp[T][i][0], dp[T][i][1]));
+            answer = Math.max(answer, dp[T][i]);
         }
         System.out.println(answer);
     }
