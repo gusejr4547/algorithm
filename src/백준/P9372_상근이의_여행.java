936a272323b702fddf69257439eafbf7811c6fa3
조현덕 <gusejr4547@gmail.com>
2026-04-15 13:57:30 +0900

    P9372_상근이의_여행

---
+++ src/백준/P9372_상근이의_여행.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P9372_상근이의_여행 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            List<Integer>[] adj = new List[N + 1];
+            for (int i = 1; i <= N; i++) {
+                adj[i] = new ArrayList<>();
+            }
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+
+            System.out.println(N-1);
+        }
+    }
+}
