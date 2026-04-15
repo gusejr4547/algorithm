5d9bc65c230d32814eb7f164f3ffa6a4e33f0fc6
조현덕 <gusejr4547@gmail.com>
2026-04-15 16:27:11 +0900

    P9372_상근이의_여행

--- src/백준/P9372_상근이의_여행.java
+++ src/백준/P9372_상근이의_여행.java
@@ -2,19 +2,24 @@ package 백준;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class P9372_상근이의_여행 {
+    static boolean[] visit;
+    static List<Integer>[] adj;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            List<Integer>[] adj = new List[N + 1];
+            adj = new List[N + 1];
             for (int i = 1; i <= N; i++) {
                 adj[i] = new ArrayList<>();
             }
@@ -26,7 +31,31 @@ public class P9372_상근이의_여행 {
                 adj[b].add(a);
             }
 
-            System.out.println(N-1);
+//            System.out.println(N-1);
+
+            visit = new boolean[N + 1];
+            sb.append(bfs(1)).append('\n');
         }
+        System.out.println(sb);
+    }
+
+    private static int bfs(int start) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visit[start] = true;
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int next : adj[cur]) {
+                if (!visit[next]) {
+                    visit[next] = true;
+                    queue.offer(next);
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
