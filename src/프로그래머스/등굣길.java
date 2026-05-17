6999e9a43c27d9e21d58d6b12183791174092557
조현덕 <gusejr4547@gmail.com>
2026-05-17 12:48:00 +0900

    등굣길

--- src/프로그래머스/등굣길.java
+++ src/프로그래머스/등굣길.java
@@ -12,46 +12,83 @@ public class 등굣길 {
         System.out.println(Main.solution(m, n, puddles));
     }
 
+    int MOD = 1_000_000_007;
+
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n][m];
+        boolean[][] block = new boolean[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 0; i < puddles.length; i++) {
-            dp[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
+        for (int[] puddle : puddles) {
+            block[puddle[1]][puddle[0]] = true;
         }
-        dp[0][0] = 1;
 
-        int sum = 1;
-        int max = m + n - 2;
-        while (sum <= max) {
-            int minI = sum >= m ? sum - (m - 1) : 0;
-            int maxI = Math.min(sum, n - 1);
-            for (int i = minI; i <= maxI; i++) {
-                int j = sum - i;
+        // 오른쪽과 아래쪽으로만 움직인다.
+        // (1,1) -> (n,m)
+        dp[1][1] = 1;
 
-                if (dp[i][j] == -1) {
+        // 위쪽 왼쪽 경로 더한거
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                if (block[i][j]) {
                     continue;
                 }
 
-                int a = 0, b = 0;
-                if (j - 1 >= 0) {
-                    a = dp[i][j - 1] == -1 ? 0 : dp[i][j - 1];
-                }
-                if (i - 1 >= 0) {
-                    b = dp[i - 1][j] == -1 ? 0 : dp[i - 1][j];
-                }
-//                System.out.println("a, b = " + a + ", " + b);
-                dp[i][j] = (a + b) == 0 ? -1 : (a + b) % 1_000_000_007;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
             }
-
-            sum++;
-//            System.out.println("=================");
-//            for (int[] qqq : dp) {
-//                System.out.println(Arrays.toString(qqq));
-//            }
-//            System.out.println("=================");
         }
 
+        // for(int[] d : dp){
+        //     System.out.println(Arrays.toString(d));
+        // }
 
-        return dp[n - 1][m - 1] == -1 ? 0 : dp[n - 1][m - 1];
+//        System.out.println(dp[n][m]);
+
+        return dp[n][m];
     }
+
+//    public int solution(int m, int n, int[][] puddles) {
+//        int[][] dp = new int[n][m];
+//
+//        for (int i = 0; i < puddles.length; i++) {
+//            dp[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
+//        }
+//        dp[0][0] = 1;
+//
+//        int sum = 1;
+//        int max = m + n - 2;
+//        while (sum <= max) {
+//            int minI = sum >= m ? sum - (m - 1) : 0;
+//            int maxI = Math.min(sum, n - 1);
+//            for (int i = minI; i <= maxI; i++) {
+//                int j = sum - i;
+//
+//                if (dp[i][j] == -1) {
+//                    continue;
+//                }
+//
+//                int a = 0, b = 0;
+//                if (j - 1 >= 0) {
+//                    a = dp[i][j - 1] == -1 ? 0 : dp[i][j - 1];
+//                }
+//                if (i - 1 >= 0) {
+//                    b = dp[i - 1][j] == -1 ? 0 : dp[i - 1][j];
+//                }
+////                System.out.println("a, b = " + a + ", " + b);
+//                dp[i][j] = (a + b) == 0 ? -1 : (a + b) % 1_000_000_007;
+//            }
+//
+//            sum++;
+////            System.out.println("=================");
+////            for (int[] qqq : dp) {
+////                System.out.println(Arrays.toString(qqq));
+////            }
+////            System.out.println("=================");
+//        }
+//
+//
+//        return dp[n - 1][m - 1] == -1 ? 0 : dp[n - 1][m - 1];
+//    }
 }
