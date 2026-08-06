f1ec30019d7439745c8452cbfcd4ae18c0d8c7dc
조현덕 <gusejr4547@gmail.com>
2026-08-06 15:23:53 +0900

    타겟_넘버 최적화

--- src/프로그래머스/타겟_넘버.java
+++ src/프로그래머스/타겟_넘버.java
@@ -1,38 +1,59 @@
 package 프로그래머스;
 
+import java.util.Arrays;
+
 public class 타겟_넘버 {
     public static void main(String[] args) {
 
     }
 
-    int answer;
+    int[][] memo;
+    int offset;
 
     public int solution(int[] numbers, int target) {
-        answer = 0;
 
         // numbers.length <= 20
         // numbers[] <= 50
         // target <= 1000
 
         // 전부 해보면 2^20
+        // 중복되는거 memo
 
-        dfs(0, 0, numbers, target);
+        int totalSum = 0;
+        for (int n : numbers) {
+            totalSum += n;
+        }
+        offset = totalSum;
 
-        return answer;
-    }
-
-    private void dfs(int idx, int cur, int[] numbers, int target) {
-        if (idx == numbers.length) {
-            if (cur == target) {
-                answer++;
-            }
-            return;
+        memo = new int[numbers.length][totalSum * 2 + 1];
+        for (int i = 0; i < numbers.length; i++) {
+            Arrays.fill(memo[i], -1);
         }
 
+        return dfs(0, 0, numbers, target);
+    }
+
+    private int dfs(int idx, int cur, int[] numbers, int target) {
+        if (idx == numbers.length) {
+            if (cur == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        int sumIdx = cur + offset;
+
+        if (memo[idx][sumIdx] != -1) {
+            return memo[idx][sumIdx];
+        }
+
+
         // +
-        dfs(idx + 1, cur + numbers[idx], numbers, target);
+        int plus = dfs(idx + 1, cur + numbers[idx], numbers, target);
 
         // -
-        dfs(idx + 1, cur - numbers[idx], numbers, target);
+        int minus = dfs(idx + 1, cur - numbers[idx], numbers, target);
+
+        return memo[idx][sumIdx] = plus + minus;
     }
 }
